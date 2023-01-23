1cf82eb58e114462ca7c83be9df47517d7a0ee11
luke rushton <lukerushton@uvic.ca>
2023-01-22 22:25:28 -0800

    minor documentation

--- CatDog Application/Controllers/CatDogController.cs
+++ CatDog Application/Controllers/CatDogController.cs
@@ -9,6 +9,7 @@ namespace CatDog_Application.Controllers
     public class CatDogController : ControllerBase
     {
 
+        //takes in a JSON payload, converts it to a string, replaces any instances of "dog" with "cat", then converts back to a JSON Object and returns it in the response body
         [HttpPost]
         public JObject PostTodoItem(JObject JsonPayload)
         {
